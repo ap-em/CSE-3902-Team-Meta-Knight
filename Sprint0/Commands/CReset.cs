cbf0e6cdc655db31aedba11b1313c583e48cd323
Owen Tishenkel <tishenkel.4@buckeyemail.osu.edu>
2021-09-18 14:13:16 -0400

    CReset implemented, will require list of commands setup to reset individual aspects of game in order to function

--- Sprint0/Commands/CReset.cs
+++ Sprint0/Commands/CReset.cs
@@ -7,9 +7,22 @@ namespace Sprint0.Commands
 {
     class CReset : ICommand
     {
+        /*Commands that must be executed in order to reset the state of the game, 
+         * preferably in the order they need to be executed.*/
+        private List<ICommand> resetCommands;
+        
+
+        public CReset(List<ICommand>resetCommands)
+        {
+            this.resetCommands = resetCommands;
+        }
+
         public void Execute()
         {
-            throw new NotImplementedException();
+            for(int index=0; index<resetCommands.Count; index++)
+            {
+                resetCommands[index].Execute();
+            }
         }
     }
 }
