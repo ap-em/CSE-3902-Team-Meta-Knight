7e4e80f83d51866331a0ed18a8c6486732cd1c8e
Owen Tishenkel <tishenkel.4@buckeyemail.osu.edu>
2021-09-24 16:23:26 -0400

    created classes for the individual reset commands that CReset will call.

---
+++ Sprint0/Commands/CResetItem.cs
@@ -0,0 +1,24 @@
+﻿using Sprint0.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sprint0.Commands
+{
+    /*Owen Tishenkel 2021 CSE 3902*/
+    class CResetItem : ICommand
+    {
+
+
+        /*CResetItem is a subcommand for CReset which resets the item displayed to the original item*/
+        public CResetItem()
+        {
+            
+        }
+
+        public void Execute()
+        {
+           
+        }
+    }
+}
---
+++ Sprint0/Commands/CResetPlayer.cs
@@ -0,0 +1,24 @@
+﻿using Sprint0.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sprint0.Commands
+{
+    /*Owen Tishenkel 2021 CSE 3902*/
+    class CResetPlayer : ICommand
+    {
+
+
+        /*CReset Player is a subcommand for CReset which resets player location and health to the initial value.*/
+        public CResetPlayer()
+        {
+            
+        }
+
+        public void Execute()
+        {
+           
+        }
+    }
+}
