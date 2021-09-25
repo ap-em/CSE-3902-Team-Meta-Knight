37218a7e9bf27c1b5fc0e38c734b85d7cd64926f
Owen Tishenkel <tishenkel.4@buckeyemail.osu.edu>
2021-09-25 19:09:17 -0400

    Interface for Item State Machine created, item class created

--- Sprint0/Items/Item.cs
+++ Sprint0/Items/Item.cs
@@ -3,20 +3,16 @@ using System.Collections.Generic;
 using System.Text;
 
 /*
- * Alexander Clayton CSE 3902 09/15/2021
- * Modified by Owen Tishenkel CSE 3902 9/25/2021
+ * Owen Tishenkel CSE 3902 
  */
-namespace Sprint0.Interfaces
+namespace Sprint0.Items
 {
     /*
      * This interface can be used for the compass, map, key, heart container, trifroce, boomerang, bow, etc. Items that are toggled to be able to use once unlocked.
      * This interface can be used for the compass, map, key, heart container, trifroce, boomerang, bow, etc. Items that are toggled to be able to use once unlocked.
      */
-   public interface IItems
+    public interface Item : IItem
     {
-        void Update();
-        void PrevItem();
-        void NextItem();
-        void SetStateMachineSprite();
+        
     }
 }
