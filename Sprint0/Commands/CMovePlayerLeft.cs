a4f12f0c4ee34073bbec9186f1acccbb8ea4ff9e
Owen Tishenkel <owen.tishenkel@gmail.com>
2021-09-29 16:13:20 -0400

    command modifications for player movement

--- Sprint0/Commands/CMovePlayerLeft.cs
+++ Sprint0/Commands/CMovePlayerLeft.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace Sprint0.Commands
 {
-    /*Owen Tishenkel 2021 CSE 3902*/
+    /*Owen Tishenkel 2021 CSE 3902 Commit pls*/
     class CMovePlayerLeft : ICommand
     {
          private Game0 gameHere;
