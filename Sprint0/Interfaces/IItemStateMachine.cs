0c014b7ae96bacd9bbef5d56182a48695dd41915
Owen Tishenkel <owen.tishenkel@gmail.com>
2021-10-01 17:54:43 -0400

    Item StateMachine modded to not have 3 different duplicate classes, also uses SpriteFactory now

--- Sprint0/Interfaces/IItemStateMachine.cs
+++ Sprint0/Interfaces/IItemStateMachine.cs
@@ -4,6 +4,7 @@ using System.Text;
 
 /*
  * Alexander Clayton CSE 3902 09/15/2021
+ * Modified by Owen Tishenkel CSE 3902 10/1/2021
  */
 namespace Sprint0.Interfaces
 {
@@ -12,6 +13,5 @@ namespace Sprint0.Interfaces
         void Update();
         void PrevItem();
         void NextItem();
-        void SetSprite();
     }
 }
--- Sprint0/Interfaces/IItems.cs
+++ Sprint0/Interfaces/IItems.cs
@@ -5,6 +5,7 @@ using System.Text;
 /*
  * Alexander Clayton CSE 3902 09/15/2021
  * Modified by Owen Tishenkel CSE 3902 9/25/2021
+ * Modified again by Owen Tishenkel CSE 3902 10/1/2021
  */
 namespace Sprint0.Interfaces
 {
@@ -17,8 +18,7 @@ namespace Sprint0.Interfaces
         void Update();
         void PrevItem();
         void NextItem();
-        void SetStateMachineSprite();
-        void setSprite(ISprite sprite);
+        void setSprite();
         void Draw();
         void Move(int x, int y);
     }
