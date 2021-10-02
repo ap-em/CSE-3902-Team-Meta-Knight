30aa0f15926d90ad06649ca911a6b7a931947b6d
LeonCai1 <70208314+LeonCai1@users.noreply.github.com>
2021-10-02 00:14:01 -0400

    Update LeftFacingMovingLink.cs

--- Sprint0/Concrete Classes/State Machines/States/LeftFacingMovingLink.cs
+++ Sprint0/Concrete Classes/State Machines/States/LeftFacingMovingLink.cs
@@ -2,7 +2,14 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-
+/*
+Alex Clayton
+Alex Contreras
+Jared Israel
+Leon Cai
+Owen Tishenkel
+Owen Huston
+*/
 namespace Sprint0
 {
     public class LeftFacingMovingLink : ILinkState
