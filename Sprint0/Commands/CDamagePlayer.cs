b5372903de7e95b10ec93be8a250aed5007efdc1
LeonCai1 <70208314+LeonCai1@users.noreply.github.com>
2021-10-02 00:06:31 -0400

    Update CDamagePlayer.cs

--- Sprint0/Commands/CDamagePlayer.cs
+++ Sprint0/Commands/CDamagePlayer.cs
@@ -3,7 +3,14 @@ using Sprint0.Interfaces.Player;
 using System;
 using System.Collections.Generic;
 using System.Text;
-
+/*
+Alex Clayton
+Alex Contreras
+Jared Israel
+Leon Cai
+Owen Tishenkel
+Owen Huston
+*/
 namespace Sprint0.Commands
 {
     /*Owen Tishenkel 2021 CSE 3902*/
