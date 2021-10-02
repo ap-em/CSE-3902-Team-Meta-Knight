48c11edf706bd05289e57925ac75cc4cba881f31
LeonCai1 <70208314+LeonCai1@users.noreply.github.com>
2021-10-02 00:18:54 -0400

    Update IEntity.cs

--- Sprint0/Interfaces/IEntity.cs
+++ Sprint0/Interfaces/IEntity.cs
@@ -1,7 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+/*
+Alex Clayton
+Alex Contreras
+Jared Israel
+Leon Cai
+Owen Tishenkel
+Owen Huston
+*/
 /*
  * Alexander Clayton CSE 3902 09/15/2021
  */
