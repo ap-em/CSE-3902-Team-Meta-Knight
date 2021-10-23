679bd507a04de0f07d9e1c2fdaa20f1fc39c9804
ap-em <alexander.cclayton1@gmail.com>
2021-10-23 19:55:48 -0400

    Added some item blocks, not necessarily working but they are in the game now.

---
+++ Sprint0/Commands/CUseItemBlock.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Sprint0.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Sprint0.Commands
+{
+    class CUseItemBlock : ICommand
+    {
+        IBlock Block;
+        public CUseItemBlock(IBlock block, Rectangle rectangle)
+        {
+        Block = block;
+        }
+        public void Execute()
+        {
+            Debug.WriteLine("Debug test hit bottom of item block");
+            
+        }
+    }
+}
