d497ddc534ae5ba11cb7011a2e154eedd5c8db17
Jared Israel <jared.seattle@gmail.com>
2021-11-01 23:01:48 -0400

    Created enemy health state machine, sprite/enemy behavior not yet linked with damage

---
+++ Sprint0/Commands/CDamageEnemy.cs
@@ -0,0 +1,21 @@
+﻿using Sprint0.Enemies;
+using Sprint0.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sprint0.Commands
+{
+    class CDamageEnemy : ICommand
+    {
+        private Enemy enemy;
+        public CDamageEnemy(Enemy enemyRefernce)
+        {
+            enemy = enemyRefernce;
+        }
+        public void Execute()
+        {
+            enemy.TakeDamage();
+        }
+    }
+}
---
+++ Sprint0/Enemies/EnemyHealthStateMachine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sprint0.Enemies
+{
+    class EnemyHealthStateMachine
+    {
+        private enum enemyHealth { alive, dead};
+        private enemyHealth health;
+        private Enemy enemy;
+        public EnemyHealthStateMachine(Enemy enemyRef)
+        {
+            health = enemyHealth.alive;
+            enemy = enemyRef;
+        }
+        public void TakeDamage()
+        {
+            switch (health)
+            {
+                case enemyHealth.alive:
+                    health = enemyHealth.dead;
+                    /*
+                     * What functions need to be called here to kill the enemy?
+                     */
+                    break;
+                case enemyHealth.dead:
+                    //No op
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
