601c89dbdea62a35855aeba0ef8d479fdb173452
Owen Huston <owenhuston32@yahoo.com>
2021-11-11 16:50:21 -0500

    added comments to collision response

--- Sprint0/Collision/CollisionResponse.cs
+++ Sprint0/Collision/CollisionResponse.cs
@@ -59,24 +59,25 @@ namespace Sprint0
             List<String> obj1commands = new List<String>();
             List<String> obj2commands = new List<String>();
 
-            XmlReader initialReader = reader;
-            XmlReader subReader = initialReader;
+            //get object1
+            if (reader.ReadToFollowing("obj1"))
+                obj1 = reader.ReadElementContentAsString();
 
-            if (subReader.ReadToFollowing("obj1"))
-                obj1 = subReader.ReadElementContentAsString();
-
-            if (subReader.ReadToFollowing("obj2"))
+            //get list of collided objects that share the same collision response
+            if (reader.ReadToFollowing("obj2"))
             {
-                obj2String = subReader.ReadElementContentAsString();
+                obj2String = reader.ReadElementContentAsString();
                 obj2Array = obj2String.Split(",");
             }
 
-            if (subReader.ReadToFollowing("direction"))
-                direction = subReader.ReadElementContentAsString();
+            // get direction
+            if (reader.ReadToFollowing("direction"))
+                direction = reader.ReadElementContentAsString();
 
-            if (subReader.ReadToFollowing("obj1Command"))
+            // get list of commands that apply to obj1
+            if (reader.ReadToFollowing("obj1Command"))
             {
-                obj1CommandString = subReader.ReadElementContentAsString();
+                obj1CommandString = reader.ReadElementContentAsString();
 
                 String[] commands = obj1CommandString.Split(",");
                 for (int i = 0; i < commands.Length; i++)
@@ -85,9 +86,10 @@ namespace Sprint0
                 }
             }
 
-            if (subReader.ReadToFollowing("obj2Command"))
+            //get list of commmands that apply to obj2
+            if (reader.ReadToFollowing("obj2Command"))
             {
-                obj2CommandString = subReader.ReadElementContentAsString();
+                obj2CommandString = reader.ReadElementContentAsString();
 
                 String[] commands = obj2CommandString.Split(",");
                 for (int i = 0; i < commands.Length; i++)
@@ -96,13 +98,13 @@ namespace Sprint0
                 }
             }
 
+            // for each object in list of collided objects
+            // add key and corresponding command
             for(int i = 0; i < obj2Array.Length; i++)
             {
                 MoverResponse.Add(obj1 + obj2Array[i] + direction, obj1commands);
                 TargetResponse.Add(obj2Array[i] + obj1 + direction, obj2commands);
             }
-
-            subReader.Close();
             reader.Close();
         }
         public void CollisionOccurrence(IGameObject collider, IGameObject collided, String direction, Rectangle rectangle)
@@ -110,6 +112,8 @@ namespace Sprint0
             List<String> commandNames1 = new List<String>();
             List<String> commandNames2 = new List<String>();
 
+            // get command1
+            // if command get value with direction fails try getting directionless command
             if (MoverResponse.TryGetValue((collider.ToString() + collided.ToString() + direction), out commandNames1)
                 || MoverResponse.TryGetValue((collider.ToString() + collided.ToString()), out commandNames1))
             {
@@ -128,7 +132,9 @@ namespace Sprint0
                 }
 
             }
-            
+
+            // get command2
+            // if command get value with direction fails try getting directionless command
             if (TargetResponse.TryGetValue((collided.ToString() + collider.ToString() + direction), out commandNames2)
                 || TargetResponse.TryGetValue((collided.ToString() + collider.ToString()), out commandNames2))
             {
