6f8ca3178d706adc76e7c0302b1686dc133bd96f
LeonCai1 <70208314+LeonCai1@users.noreply.github.com>
2021-11-12 02:43:13 -0500

    Update gameHUD.cs

--- Sprint0/HUD/gameHUD.cs
+++ Sprint0/HUD/gameHUD.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Sprint0.Interfaces;
+using Sprint0.Player.State_Machines.States.GameStates;
 using Sprint0.UtilityClasses;
 namespace Sprint0.HUD
 {
@@ -12,26 +13,31 @@ namespace Sprint0.HUD
         private int time;
         private bool isGameOver = false;
         private bool timeUp = false;
-        public gameHUD(Mario mario)
+        private int counter = 0;
+       
+        public gameHUD()
         {
             Score.marioScore = 0;
             Score.coins = 0;
             time = GameUtilities.startTime;
+         
 
         }
             
-        public void draw(SpriteBatch batch, SpriteFont font)
+        public void draw(SpriteBatch spriteBatch, SpriteFont font)
         {
-             batch.Begin();
-            batch.DrawString(font, GameUtilities.MARIO + GameUtilities.TAB + GameUtilities.COINS + GameUtilities.TAB + GameUtilities.LIVES + GameUtilities.TAB + GameUtilities.TIME+ GameUtilities.TAB + GameUtilities.WORLD, new Vector2(10, 10), Color.White);
+            
+            spriteBatch.DrawString(font, GameUtilities.MARIO + GameUtilities.TAB + GameUtilities.COINS + GameUtilities.TAB + GameUtilities.LIVES + GameUtilities.TAB + GameUtilities.TIME+ GameUtilities.TAB + GameUtilities.WORLD, new Vector2(10, 10), Color.White);
 
-            batch.DrawString(font, Score.marioScore.ToString() + GameUtilities.TAB +Score.coins.ToString() + GameUtilities.TAB +Score.lives.ToString() + GameUtilities.TAB + time.ToString() + GameUtilities.TAB + "1-1", new Vector2(10, 25), Color.White);
+            spriteBatch.DrawString(font, scoreTextFormat(Score.marioScore) + GameUtilities.TAB + coinsTextFormat(Score.coins) + GameUtilities.TAB + GameUtilities.TAB+ livesTextFormat(Score.lives) + GameUtilities.TAB  +GameUtilities.TAB+GameUtilities.TAB+ GameUtilities.TAB + time.ToString() + GameUtilities.TAB+ GameUtilities.TAB + "1-1", new Vector2(10, 25), Color.White);
             // TO DO: Test the format of the HUD
 
             if (isGameOver)
             {
                 // TO DO: draw game over;
-                batch.DrawString(font, GameUtilities.GAMEOVER, new Vector2(GraphicsDeviceManager.DefaultBackBufferWidth / 2, GraphicsDeviceManager.DefaultBackBufferHeight / 2), Color.White);
+                GameOverState.Instance.Draw(spriteBatch);
+                GameOverState.Instance.GameOver();
+                //spriteBatch.DrawString(font, GameUtilities.GAMEOVER, new Vector2(GraphicsDeviceManager.DefaultBackBufferWidth / 2, GraphicsDeviceManager.DefaultBackBufferHeight / 2), Color.White);
                 time = GameUtilities.startTime;
                 Score.marioScore = 0;
                 Score.coins = 0;
@@ -39,16 +45,24 @@ namespace Sprint0.HUD
             }
             else if (timeUp)
             {
-                batch.DrawString(font, GameUtilities.TIMEUP, new Vector2(GraphicsDeviceManager.DefaultBackBufferWidth / 2, GraphicsDeviceManager.DefaultBackBufferHeight / 2), Color.White);
+                spriteBatch.DrawString(font, GameUtilities.TIMEUP, new Vector2(GraphicsDeviceManager.DefaultBackBufferWidth / 2, GraphicsDeviceManager.DefaultBackBufferHeight / 2), Color.White);
                 time = GameUtilities.startTime;
                 Score.marioScore = 0;
                 Score.coins = 0;
             }
-            // TO DO: game complete
+          
+         
         }
 
-        public void update()
+        public void Update()
         {
+            // count the time 
+            counter++;
+            if (counter == GameUtilities.FPS)
+            {
+                time--;
+                counter = 0;
+            }
             if(time < 60)
             {
                 // play hurry sound
@@ -72,17 +86,41 @@ namespace Sprint0.HUD
 
         }
        private string  scoreTextFormat(int score) {
-            string text = "";
+           
+            int gameScore = score;
+            String text = "";
+            for (int i = 0; i < 6; i++)
+            {
+                text = (gameScore % 10) + text;
+                gameScore /= 10;
+            }
             return text;
         }
         private string coinsTextFormat(int coins)
         {
             string text = "";
+            if (coins < 10)
+            {
+                text = "0" + coins.ToString();
+            }
+            else
+            {
+                text = coins.ToString();
+            }
             return text;
         }
         private string livesTextFormat(int lives)
         {
             string text = "";
+            if (lives < 10)
+            {
+                text = "0" + lives.ToString();
+            }
+            else
+            {
+                text = lives.ToString();
+            }
+          
             return text;
         }
     }
