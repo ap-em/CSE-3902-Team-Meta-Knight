826f7d88622dd57491f9486e53833430d8ca6994
Owen Huston <owenhuston32@yahoo.com>
2021-12-05 11:49:25 -0500

    fixed game over level switch

--- Sprint0/HUD/HUD.cs
+++ Sprint0/HUD/HUD.cs
@@ -15,8 +15,8 @@ namespace Sprint0.HUD
         private IHUDState hudState;
         private float initialPlayerPosition;
         private float maxPlayerPosition;
-        private int initialLives = 3;
-        private int lives = 3;
+        private int initialLives = 1;
+        private int lives = 1;
         private int initialScore = 0;
         private int initialCoinCount = 0;
         private int coinCount = 0;
@@ -109,14 +109,7 @@ namespace Sprint0.HUD
             ResetLevel();
             lives = initialLives;
             hudState = new DefaultHUD(gameObject, this);
-        }
-        public int GetLevel()
-        {
-            return level;
-        }
-        public void SetLevel(int level)
-        {
-            this.level = level;
+
         }
         public void SetMaxPlayerPosition(float position)
         {
